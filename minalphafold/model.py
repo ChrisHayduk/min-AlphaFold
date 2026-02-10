0fc86802f213fba6da67575ababbd24d1237e0e6
Chris Hayduk <chayduk@fordham.edu>
2026-02-10 18:25:38 -0500

    Update model.py

--- minalphafold/model.py
+++ minalphafold/model.py
@@ -1,4 +1,27 @@
 """
 Full definition of the AlphaFold2 model, all of it in this single file.
 """
+from torch import nn, Tensor
+class Evoformer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        pass
 
+    def forward(self, msa_representation: Tensor, pair_representation: Tensor):
+        pass
+    
+class StructureModule(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        pass
+
+    def forward(self, single_representation: Tensor, pair_representation: Tensor):
+        pass
+
+class AlphaFold2(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        pass
+
+    def forward(self, msa_representation: Tensor, pair_representation: Tensor):
+        pass
